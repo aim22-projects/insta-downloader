363113a257bd8f1aca1701018f3ac726fea80559
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-29 09:11:36 +0530

    Dialog example is added in HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, Text, useColorScheme } from 'react-native';
-import { MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper';
+import { SafeAreaView, ScrollView, StatusBar, useColorScheme } from 'react-native';
+import { Button, Dialog, MD3DarkTheme, MD3LightTheme, PaperProvider, Portal, Text } from 'react-native-paper';
 
 const themes = {
   lightTheme: {
@@ -24,11 +24,28 @@ function App(): JSX.Element {
 function HomePage(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = { backgroundColor: isDarkMode ? '#323232' : '#e1e1e1' };
+  const [visible, setVisible] = React.useState(false);
+
+  const showDialog = () => setVisible(true);
+
+  const hideDialog = () => setVisible(false);
 
   return (<SafeAreaView style={backgroundStyle}>
+    <Portal>
+      <Dialog visible={visible} onDismiss={hideDialog}>
+        <Dialog.Title>Alert</Dialog.Title>
+        <Dialog.Content>
+          <Text variant="bodyMedium">This is simple dialog</Text>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={hideDialog}>Done</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
     <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={backgroundStyle.backgroundColor} />
     <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
       <Text>App.tsx</Text>
+      <Button onPress={showDialog}>Show Dialog</Button>
     </ScrollView>
   </SafeAreaView>);
 }
