82a4ff834ded5a5973c35d003329575e9ca44887
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-29 17:50:25 +0530

    Dialog is seperated as SampleDialog component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
 import { Button, Dialog, MD3DarkTheme, MD3LightTheme, PaperProvider, Portal, Text } from 'react-native-paper';
 import useDarkMode from './hooks/useDarkMode';
 import useVisibility from './hooks/useVisibility';
@@ -28,7 +28,20 @@ function HomePage(): JSX.Element {
   const backgroundStyle = { backgroundColor: isDarkMode ? '#323232' : '#e1e1e1' };
   const [visible, showDialog, hideDialog] = useVisibility(false);
 
-  return (<SafeAreaView style={backgroundStyle}>
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={backgroundStyle.backgroundColor} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+        <Text>App.tsx</Text>
+        <Button onPress={showDialog}>Show Dialog</Button>
+        <SampleDialog visible={visible} hideDialog={hideDialog} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+function SampleDialog({ visible, hideDialog }: { visible: boolean, hideDialog: () => void }): JSX.Element {
+  return (
     <Portal>
       <Dialog visible={visible} onDismiss={hideDialog}>
         <Dialog.Title>Alert</Dialog.Title>
@@ -40,12 +53,7 @@ function HomePage(): JSX.Element {
         </Dialog.Actions>
       </Dialog>
     </Portal>
-    <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={backgroundStyle.backgroundColor} />
-    <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-      <Text>App.tsx</Text>
-      <Button onPress={showDialog}>Show Dialog</Button>
-    </ScrollView>
-  </SafeAreaView>);
+  );
 }
 
 export default App;
