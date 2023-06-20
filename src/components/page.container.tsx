e5a8d8a6ab5b3c1df9581d488289392cd8ad5acc
smkcegovernance <smkcegovernance@gmail.com>
2023-06-20 12:52:03 +0530

    Automatic dark mode switching is implemented

--- src/components/page.container.tsx
+++ src/components/page.container.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { StatusBar, View } from "react-native";
+import { StatusBar, View, useColorScheme } from "react-native";
 import { useTheme } from "react-native-paper/src/core/theming";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -10,10 +10,13 @@ interface IPageContainer {
 
 export default function PageContainer({ children, safeArea }: IPageContainer) {
     const { colors } = useTheme();
+    const colorScheme = useColorScheme();
+
     const SelectedView = safeArea ? SafeAreaView : View;
+    const barStyle = colorScheme === 'dark' ? 'light-content' : 'dark-content';
     return (
         <SelectedView style={{ flex: 1, backgroundColor: colors.background }}>
-            <StatusBar barStyle={'dark-content'} />
+            <StatusBar barStyle={barStyle} />
             {children}
         </SelectedView>
     );
